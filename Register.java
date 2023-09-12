e2feb76b9d7471a7b1d071f6c707b108072daab6
Adi Duggal <duggaadi25@7hills.org>
2023-09-12 12:40:38 -0400

    Created Transaction and Register

---
+++ Register.java
@@ -0,0 +1,11 @@
+public class Register {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+    
+
+}
---
+++ Transaction.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+    //main interface
+public class Transaction {
+    public String custID;
+    public String item1;
+    public double price1;
+
+
+
+
+
+}
