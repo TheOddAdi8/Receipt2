bdd1b2f36f347c20452a8bc799954899fd43ff9e
Adi Duggal <duggaadi25@7hills.org>
2023-09-12 13:08:34 -0400

    Made working program, assigned a problem for hw

--- Register.java
+++ Register.java
@@ -1,10 +1,12 @@
 public class Register {
     public static void main(String[] args) {
 
+        Transaction t1 = new Transaction();
+        while (!fin) {
+            finished = t1.nextCustomer()
 
-
-
-
+        }
+        
     }
     
 
--- Transaction.java
+++ Transaction.java
@@ -1,11 +1,37 @@
 import java.util.Scanner;
 
     //main interface
+    //creat variables
+    //create methods
+    //(create a constructor)
+
 public class Transaction {
     public String custID;
-    public String item1;
-    public double price1;
+    public String item1, item2;
+    public double price1, price2;
+    public boolean done = false;
+    public boolean fin = false;
 
+    public boolean nextCustomer() {
+        greet();
+        while (!done) {
+            scanItems();
+        }
+        checkOut();
+        return fin;
+    }
+    public void greet() {
+        System.out.println("Welcome to Adi's Convenience Store!");
+    }
+    public void scanItems() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Scan the next item");
+        }
+        done = true;
+    }
+    public void checkOut() {
+        System.out.println("Pay me or else :)");
+    }
 
 
 
