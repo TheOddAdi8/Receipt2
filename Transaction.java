db8083443442a14beae2e21837f384b863ffa343
Adi Duggal <123963842+TheOddAdi8@users.noreply.github.com>
2023-09-13 19:27:02 -0400

    Update Transaction.java
    made Transaction.java interactive -- still needs changes

--- Transaction.java
+++ Transaction.java
@@ -6,26 +6,47 @@ import java.util.Scanner;
     //(create a constructor)
 
 public class Transaction {
-    public String custID;
+    public int custID;
     public String item1, item2;
     public double price1, price2;
     public boolean done = false;
-    public boolean fin = false;
-
+    public String tempGreet;
+    public Scanner storeBot = new Scanner(System.in);
+    
     public boolean nextCustomer() {
         greet();
         while (!done) {
             scanItems();
         }
         checkOut();
-        return fin;
+        done = true;
+        return done;
     }
     public void greet() {
         System.out.println("Welcome to Adi's Convenience Store!");
+        
+        checkID();
+    }
+    public void checkID() {
+        System.out.println("Do you have a Customer ID?");
+        tempGreet = storeBot.nextLine();
+
+        if (tempGreet.toLowerCase().contains("no")) {
+            System.out.println("The creator of this program was too lazy to actually make this part so you can just move on and acually buy the things now :)");
+        }
+        else if (tempGreet.toLowerCase().contains("yes")) {
+            System.out.print("Enter your Customer ID: ");
+            storeBot.nextInt();
+            custID = storeBot.nextInt();
+        }
+        else {
+            System.out.println("I'm sorry, but I cannot understand you. Please try again.");
+            checkID();
+        }
     }
     public void scanItems() {
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Scan the next item");
+        while (!done) {
+            System.out.print("Scan the next item: ");
         }
         done = true;
     }
