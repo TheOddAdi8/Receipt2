2b0005d8b0bddf3710ec4fdbf0842a0e3c33e692
Adi Duggal <duggaadi25@7hills.org>
2023-09-14 12:34:30 -0400

    Old MacDonald?

---
+++ OldMacDonald.java
@@ -0,0 +1,6 @@
+public class OldMacDonald {
+    public void sing(String animal, String sound) {
+        System.out.println("\n");
+        System.out.println("Old MacDonald had a farm\nE-I-E-I-O\nAnd on his farm he had a " + animal + "\nE-I-E-I-O\nWith a " + sound + " " + sound + " here and a " + sound + " " + sound + " there\nHere a " + sound + " there a " + sound + "\nEverywhere a " + sound + " " + sound + "\nOld MacDonald had a farm\nE-I-E-I-O");
+    }
+}
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+    public static void main(String[] args) {
+        OldMacDonald farmer = new OldMacDonald();
+        farmer.sing("pig", "oink");
+        farmer.sing("cow", "moo");
+    }
+}
